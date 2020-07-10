dbbafe5548b3c7465913012f34b3b11edf202924
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-07-10 15:33:48 +0200

    Update SimpleWebServerM7.ino

--- blink-webserver/SimpleWebServerM7/SimpleWebServerM7.ino
+++ blink-webserver/SimpleWebServerM7/SimpleWebServerM7.ino
@@ -134,13 +134,13 @@ void loop() {
           digitalWrite(LEDG, LOW);                // GET /Hg turns the Green LED on
         }
         if (currentLine.endsWith("GET /Lg")){
-          digitalWrite(LEDG, HIGH);                // GET /Hg turns the Green LED on
+          digitalWrite(LEDG, HIGH);                // GET /Hg turns the Green LED off
         }
         if (currentLine.endsWith("GET /Hb")){
           digitalWrite(LEDB, LOW);                // GET /Hg turns the Blue LED on
         }
         if (currentLine.endsWith("GET /Lb")){
-          digitalWrite(LEDB, HIGH);                // GET /Hg turns the Blue LED on
+          digitalWrite(LEDB, HIGH);                // GET /Hg turns the Blue LED off
         } 
 
       }
